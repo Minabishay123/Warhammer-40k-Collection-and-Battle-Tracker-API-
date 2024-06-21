0e675498881b5035d43fdd550ed034e879dc582b
Minabishay123 <minabishay@hotmail.com>
2024-06-21 15:56:15 +1000

    continue refactering

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,2 @@
 FLASK_DEBUG=true
-FLASK_APP=src/manage.py
-FLASK_ENV=development
+
--- src/app.py
+++
@@ -1,8 +0,0 @@
-from init import app, db
-import cli  # Import CLI commands to register them with the Flask app
-
-# Import CLI commands
-app.cli.add_command(cli.db_commands)
-
-if __name__ == '__main__':
-    app.run(debug=True)
